2650d4136069eb17ff786eb784b3f8c4d4a64723
kayz <kukakthesery4@gmail.com>
2025-03-22 22:10:47 +0700

    added remove/edit(not finish yet) api

--- src/components/AllBookings.tsx
+++ src/components/AllBookings.tsx
@@ -1,6 +1,10 @@
+"use client"
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { getBookings } from "@/libs/getBookings";  // นำเข้าฟังก์ชัน getBookings จาก libs
+import { useDispatch, UseDispatch } from "react-redux"
+import { AppDispatch, useAppSelector } from "@/redux/store"
+import { deleteBookings } from "@/libs/deleteBooking";
 
 const AllBookings = () => {
 
@@ -10,6 +14,8 @@ const AllBookings = () => {
   const [bookings, setBookings] = useState<BookingData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     const fetchBookings = async () => {
@@ -42,6 +48,10 @@ const AllBookings = () => {
     return <div>{error}</div>;
   }
 
+  if(!token){
+    return <div>No Token</div>
+  }
+
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
       <h1 className="text-3xl font-semibold text-center mb-6">All Bookings</h1>
@@ -59,7 +69,11 @@ const AllBookings = () => {
               <p className="text-gray-700"><strong>Created At:</strong> {new Date(booking.createdAt).toLocaleDateString()}</p>
             </div>
             <button className="px-4 py-2 bg-red-500 text-white font-semibold rounded-lg shadow-md 
-              hover:bg-red-600 hover:shadow-lg transition duration-300 ease-in-out my-3 mx-1">
+              hover:bg-red-600 hover:shadow-lg transition duration-300 ease-in-out my-3 mx-1"
+              onClick={async () => {
+                await deleteBookings(token, booking._id); // Call API
+                setBookings(bookings.filter((b) => b._id !== booking._id)); // Remove from UI
+              }}>
               Remove
             </button>
             <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
