823816fccb4a803fa1adc66a4398b74b9de1188d
Sinsuda <sinsuda.rakporpiang@gmail.com>
2025-03-23 00:06:19 +0700

    style

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,27 +3,34 @@ import styles from './topmenu.module.css'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function TopMenu() {
     const { data: session } = useSession();
+    const pathname = usePathname();
 
     return (
         <div className={styles.menucontainer}>
-            <Link href="/" prefetch={true}>
-            <Image 
-                src={'/img/logo.png'} 
-                className={styles.logoimg} 
-                alt='logo' 
-                width={0} height={0} sizes='100vh' 
-            />
-            </Link>
+            <div className="flex items-center">
+                <Link href="/" prefetch={true}>
+                    <Image  
+                        src={'/img/logo.png'} 
+                        className={styles.logoimg} 
+                        alt='logo' 
+                        width={0} height={0} sizes='100vh' 
+                    />
+                </Link>
+                <Link href="/" prefetch={true} className={`${styles.itemcontainer} ${pathname === '/' ? styles.active : ''}`}>
+                    Home
+                </Link>
+            </div>
             <div className="flex items-center space-x-4 absolute right-0 h-full px-8">
                 {session ? (
                     <>
-                        <Link href='/booking' prefetch={true} className={styles.itemcontainer}>
+                        <Link href='/booking' prefetch={true} className={`${styles.itemcontainer} ${pathname === '/booking' ? styles.active : ''}`}>
                             Booking
                         </Link>
-                        <Link href='/mybooking' prefetch={true} className={styles.itemcontainer}>
+                        <Link href='/mybooking' prefetch={true} className={`${styles.itemcontainer} ${pathname === '/mybooking' ? styles.active : ''}`}>
                             My Booking
                         </Link>
                         <Link href="/api/auth/signout" prefetch={true} className={styles.itemcontainer + "w-auto text-cyan-600 text-md"}>
