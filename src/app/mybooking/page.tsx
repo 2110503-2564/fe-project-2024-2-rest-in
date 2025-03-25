1eb1de1ecfd75e693cbdff9cc52dbd5f416fa4eb
Puckanut Laoarpasuwong <puckanut2549@gmail.com>
2025-03-25 13:00:20 +0700

    fixed code

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import BookingList from "@/components/BookingList"
 import AllBookings from "@/components/AllBookings"
 
 export default function CartPage(){
     return (
         <main className="bg-[#FFD8A3]">
-            {/* <BookingList></BookingList> */}
             <AllBookings></AllBookings>
         </main>
     )
