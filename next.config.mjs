763104a8e5322a4abf99d63579085360f66eb851
Puckanut Laoarpasuwong <puckanut2549@gmail.com>
2025-03-25 13:31:13 +0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,29 +3,20 @@ const nextConfig = {
     images: {
         domains: ['drive.google.com']
     },
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/cars/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    },
-    async rewrites() {
-        return [
-          {
-            // สำหรับการทำให้เส้นทาง `/` นำไปที่หน้า Home (แก้ปัญหา 404)
-            source: '/',
-            destination: '/index'
-          }
-        ];
-      }
+    // async headers() {
+    //     return [
+    //         {
+    //             // matching all API routes
+    //             source: "/cars/:path*",
+    //             headers: [
+    //                 { key: "Access-Control-Allow-Credentials", value: "true" },
+    //                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+    //                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    //                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    //             ]
+    //         }
+    //     ]
+    // }
 };
 
 export default nextConfig;
